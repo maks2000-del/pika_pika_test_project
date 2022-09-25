8edde4824dadd8abc30e04c471ac006214716b35
maks2000-del <max3047380@mail.ru>
2022-09-25 13:37:43 +0300

    infopage block fixes, local data repo

--- lib/presentation/pages/detailed_info/detailed_info_page.dart
+++ lib/presentation/pages/detailed_info/detailed_info_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+
+import 'bloc/detailed_info_bloc.dart';
 
 class DetailedInfoPage extends StatelessWidget {
   final String pokemonUrl;
@@ -9,95 +12,98 @@ class DetailedInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text(pokemonUrl),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
+    return BlocBuilder<DetailedInfoBloc, DetailedInfoState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.grey[900],
+          appBar: AppBar(
+            title: Text(pokemonUrl),
+            centerTitle: true,
+            backgroundColor: Colors.grey[850],
+            elevation: 0.0,
+            leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+              ),
+              onPressed: () => Get.back(),
+            ),
           ),
-          onPressed: () => Get.back(),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                child: Image.network(
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-                ),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 1.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Max',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 1.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Text(
-              'Curren lvl',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 1.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'textlevel',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 1.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
+                Center(
+                  child: CircleAvatar(
+                    child: Image.network(
+                      state.pokemonInfo.frontImage ?? 'here',
+                      //TODO no image image
+                    ),
+                    radius: 40.0,
+                  ),
                 ),
-                SizedBox(
-                  width: 10.0,
+                Divider(
+                  height: 60.0,
+                  color: Colors.grey[800],
+                ),
+                const Text(
+                  'Name: ',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  state.pokemonInfo.name ?? '',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 1.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40.0,
+                ),
+                Text(
+                  'Curren lvl',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'textlevel',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 1.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Row(
+                  children: [
+                    Text('weihgt'),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('height')
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
